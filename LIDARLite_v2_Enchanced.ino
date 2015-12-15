e87ac98002b7b617e53cd7b04632340cc351d104
Alexis Paques <alexis.paques@gmail.com>
2015-12-15 16:12:58 +0100

    Working controller, adding Filter

--- LIDARLite_v2_Enchanced.ino
+++ LIDARLite_v2_Enchanced.ino
@@ -1,6 +1,7 @@
-#include "LidarEnchanced.h"
+#include "LidarObject.h"
+#include "LidarController.h"
+#include "I2CFunctions.h"
 #include <Wire.h>
-
 
 /*** Defines : CONFIGURATION ***/
 // Defines laser ready data
@@ -21,75 +22,134 @@
 #define Z_LASER_AD 0x68
 #define W_LASER_AD 0x6A
 
-//Concatenate enable pins into a variable
-char names[] = {'X', 'Y', 'Z', 'W'};
-// Array of pins connected to the sensor Power Enable lines
-byte sensorPinsEN[] = {X_LASER_EN, Y_LASER_EN, Z_LASER_EN, W_LASER_EN};
-//Concatenate adresses into a variable
-byte addresses[] = {X_LASER_AD, Y_LASER_AD, Z_LASER_AD, W_LASER_AD};
+#define NUMBER_OF_LASERS 4
 
 #define READINESS true
 
-LIDAREnchanced L;
+// Lidars
+static LidarController Controller;
+static LidarObject LX1;
+static LidarObject LX2;
+static LidarObject LY1;
+static LidarObject LY2;
+static KFilter Kx;
+static KFilter KX;
+static KFilter Ky;
+static KFilter KY;
 
-int oneShot(byte Lidar = 0x62){
-  L.async(Lidar);
-  while(L.isBusy(Lidar)){}
-  return L.distance(Lidar);
+void beginLidars(){
+  // Initialisation of the lidars objects
+  LX1.begin(X_LASER_EN, X_LASER_PIN, X_LASER_AD, 2, 'x');
+  LX2.begin(Y_LASER_EN, Y_LASER_PIN, Y_LASER_AD, 2, 'X');
+  LY1.begin(Z_LASER_EN, Z_LASER_PIN, Z_LASER_AD, 2, 'y');
+  LY2.begin(W_LASER_EN, W_LASER_PIN, W_LASER_AD, 2, 'Y');
+
+  // Initialisation of the controller
+  Controller.begin(false);
+
+  // Adding Lidars to the controller
+  Controller.add(&LX1, 0, &Kx);
+  Controller.add(&LX2, 1, &KX);
+  Controller.add(&LY1, 2, &Ky);
+  Controller.add(&LY2, 3, &KY);
+
+  Controller.changeAllAddresses();
 }
 
-void shotThemAll(){
-  L.async(0x62);
-  L.async(0x64);
-  L.async(0x66);
-  L.async(0x68);
-  L.async(0x6A);
-  Serial.print("data : ");
-  Serial.print(L.distance(0x62));
-  Serial.print("\t");
-  Serial.print(L.distance(0x64));
-  Serial.print("\t");
-  Serial.print(L.distance(0x66));
-  Serial.print("\t");
-  Serial.print(L.distance(0x68));
-  Serial.print("\t");
-  Serial.println(L.distance(0x6A));
-}
 
+void beginFilters(){  
+  // Initialisation of the lidars objects
+  for(byte i = 0; i < 4; i++){
+    int data[3] = {0,0,0};
+    int temp = 0;
+    delay(20);
+    Controller.distanceAndAsync(i, &data[0]);
+    delay(20);
+    Controller.distanceAndAsync(i, &data[1]);
+    delay(20);
+    Controller.distanceAndAsync(i, &data[2]);
+
+    // Sort the 3 data
+    if(data[1] < data[0]){
+      temp = data[0];
+      data[0] = data[1];
+      data[1] = temp;
+    }
+    if(data[2] < data[1]){
+      temp = data[1];
+      data[1] = data[2];
+      data[2] = temp;
+    }
+    if(data[1] < data[0]){
+      temp = data[0];
+      data[0] = data[1];
+      data[1] = temp;
+    }
+    if(data[2] < data[1]){
+      temp = data[1];
+      data[1] = data[2];
+      data[2] = temp;
+    }
+
+    Controller.filters[i]->begin(data[1], 0, 200, 1, 20);
+    Serial.println("\n Begin filters");
+    Serial.print(data[0]);
+    Serial.print("\t");
+    Serial.print(data[1]);
+    Serial.print("\t");
+    Serial.print(data[2]);
+    Serial.println("\t");
+  }
+}
 
 void setup(){
   Serial.begin(115200);
-  L.begin();
-  L.scan();
-  L.reset();
-  delay(100);
-  //changeAddress
-  delay(100);
-  L.changeAddressMultiPwrEn(4, addresses, sensorPinsEN, 2);
-  Serial.println(L.isOnline(0x62));
-  Serial.println(L.isOnline(0x64));
-  Serial.println(L.isOnline(0x66));
-  Serial.println(L.isOnline(0x68));
-  Serial.println(L.isOnline(0x6A));
+  while(!Serial);
+  beginLidars();
+  Controller.asyncAll();
+  beginFilters();
 }
 
 void loop(){
-  L.scan();
-  long start = micros();
-  //int total = oneShot(address);
-  shotThemAll();
-  long stop = micros();
-  #if READINESS
-  Serial.print("Business : ");
-  Serial.print(stop-start);
-  Serial.println(" microS ");
-  //Serial.print("measure : ");
-  //Serial.println(total);
-  #else
-  Serial.println(stop-start);
-  //Serial.print(",");
-  //Serial.println(total);
-  #endif
-
+  // Check reset lasers
+  // Reset lasers
+  //getData
+  Controller.checkAllToReset();
+  //exportData();
+  laserAcquisition();
+  delay(20);
+  // Verify data
+  // 
 }
 
+void laserAcquisition(){
+  for(byte i = 0; i < NUMBER_OF_LASERS; i++){
+    int data = 0;
+    byte nack = Controller.distanceAndAsync(i, &data);
+    double dataFiltered = Controller.filters[i]->filter(data);
+    Serial.print("s: ");
+    Serial.print(nack, BIN);
+    Serial.print("\t  ");
+    Serial.print("d: ");
+    Serial.print(data);
+    Serial.print("\t  ");
+    Serial.print("f: ");
+    Serial.print(dataFiltered);
+  }
+  Serial.println();
+}
+
+void exportData(){
+  int data = 0;
+  int nack = Controller.distanceAndAsync(0, &data);
+  double dataFiltered = Controller.filters[0]->filter(data);
+  Serial.print(nack);
+  Serial.print(",");
+  Serial.print(data);
+  Serial.print(",");
+  Serial.println(dataFiltered);
+}
+
+void resetLasers(){
+  
+}
---
+++ LidarController.h
@@ -0,0 +1,335 @@
+#ifndef LIDAR_CONTROLLER_H
+#define LIDAR_CONTROLLER_H
+
+#include "I2CFunctions.h"
+#include "LidarObject.h"
+#include "KFilter.h"
+#include <Wire.h>
+
+#define MAX_LIDARS 4
+
+class LidarController {
+  public: 
+    void begin(bool fasti2c = false){
+      resetToReset();
+      Wire.begin();
+      if (fasti2c) {
+      #if ARDUINO >= 157
+          Wire.setClock(400000UL); // Set I2C frequency to 400kHz, for the Due
+      #else
+          TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+      #endif
+      }
+    }
+
+    bool add(LidarObject* _Lidar, byte _id, KFilter* _filter){
+      if(_id >= MAX_LIDARS)
+        return false;
+      lidars[_id] = _Lidar;
+      filters[_id] = _filter;
+      setState(_id, NEED_CONFIGURE);
+      count++;
+      return true;
+    }
+
+/*******************************************************************************
+  checkTimers : 
+*******************************************************************************/
+    void checkTimers(){
+      for(byte i = 0; i < count; i++){
+        if(lidars[i]->check_timer()){
+          postReset(i);
+          setState(i,NEED_CONFIGURE);
+        }
+      }
+    };
+
+
+/*******************************************************************************
+  configure : Configure the default acquisition mode
+
+  configuration : The configuration of the Lidar
+      - 0 = basic configuration
+      - 1 = faster (Do not read 3 times), bit noisier
+      - 2 = low noise, low sensitivity, less false detection (Default)
+      - 3 = High noise, high sensitivity
+  Lidar : Address of the Lidar (0x62 by default)
+*******************************************************************************/
+    void configure(byte Lidar = 0, byte configuration = 2){
+      switch (configuration) {
+      case 0: //  Default configuration
+        I2C.write(lidars[Lidar]->address, 0x00, 0x00);
+        break;
+      case 1: //  Set aquisition count to 1/3 default value, faster reads, slightly
+        //  noisier values
+        I2C.write(lidars[Lidar]->address, 0x04, 0x00);
+        break;
+      case 2: //  Low noise, low sensitivity: Pulls decision criteria higher
+        //  above the noise, allows fewer false detections, reduces
+        //  sensitivity
+        I2C.write(lidars[Lidar]->address, 0x1c, 0x20);
+        break;
+      case 3: //  High noise, high sensitivity: Pulls decision criteria into the
+        //  noise, allows more false detections, increses sensitivity
+        I2C.write(lidars[Lidar]->address, 0x1c, 0x60);
+        break;
+      }
+      setState(Lidar, CONFIGURED);
+    };
+
+/*******************************************************************************
+  changeAddress : Change the address of one Lidar
+
+returns 0 if success
+        1 if error writing the serial number (byte 1)
+        2 if error writing the serial number (byte 2)
+        3 if error sending the Lidar address
+        4 if error disabling the main address
+        5 if the new Lidar address is already ON
+        6 if the Lidar do not respond
+*******************************************************************************/
+    byte changeAddress(byte Lidar){
+      byte _lidar_new = lidars[Lidar]->address;
+      // Return 6 = The device do not respond
+      if(!I2C.isOnline(0x62))
+        return 6;
+      // Return 5 = We already got an I2C device at this place
+      if(I2C.isOnline(_lidar_new))
+        return 5;
+      /* Serial number part */
+      unsigned char serialNumber[2];
+      I2C.readWord(0x62, 0x96, serialNumber);
+      // Return 1 = Error sending the Serial (byte 1)
+      if(I2C.nackError(I2C.write(0x62, 0x18, serialNumber[0])))
+        return 1;
+      // Return 2 = Error sending the Serial (byte 2)
+      if(I2C.nackError(I2C.write(0x62, 0x19, serialNumber[1])))
+        return 2;
+
+      // Return 3 = Error sending the Lidar address
+      if(I2C.nackError(I2C.write(0x62, 0x1a, _lidar_new)))
+        return 3;
+
+      // Return 4 = Error disabling the Lidar Main address (0x62)
+      if(I2C.nackError(I2C.write(0x62, 0x1e, 0x08)))
+        return 4;
+
+      return 0;
+    };
+
+
+/*******************************************************************************
+  changeAddressMultiPwrEn : Change multiple Lidars addresses
+  number : number of Lidar we would like to change the address
+  newLidar : Array of new Lidars addresses
+  pwrEnable : Array of new Lidars power enable lines
+  configuration : The configuration wanted (2 by default)
+*******************************************************************************/
+    void changeAllAddresses(){
+      // Shutdown them all
+      Serial.print("OFF : ");
+      for(int i = 0; i < count; i++){
+        Serial.print(lidars[i]->EnablePin, HEX);
+        Serial.print("\t");
+        pinMode(lidars[i]->EnablePin, OUTPUT);
+        delay(20);
+        lidars[i]->off();
+        delay(20);
+      }
+      // Power on them one per one,
+      // Change their address & configure them
+      Serial.print("ON : ");
+      for(int i = 0; i < count; i++){
+        Serial.print(lidars[i]->address, HEX);
+        Serial.print("\t");
+        lidars[i]->on();
+        delay(20);
+        Serial.print("\t");
+        Serial.print(changeAddress(i));
+        Serial.print("\t");
+        
+        delay(20);
+        configure(lidars[i]->configuration, lidars[i]->address);
+      }
+    }
+
+/*******************************************************************************
+  isBusy : check if the Lidar is in acquisition
+
+  returns true if busy
+          flse if ready to be read
+*******************************************************************************/
+    bool isBusy(byte Lidar = 0){
+      byte _lidar = lidars[Lidar]->address;
+      Wire.beginTransmission((byte)_lidar);
+      Wire.write(0x01);
+      Wire.requestFrom((byte)_lidar, (byte)1);
+      byte busyFlag = bitRead(Wire.read(), 0);
+      return busyFlag != 0;
+    };
+
+/*******************************************************************************
+  status : check the status of the Lidar
+
+  returns the status register (0x01 for version 21 of the Lidar Software)
+*******************************************************************************/
+    byte status(byte Lidar = 0){
+      byte data[1] = {0};
+      I2C.readByte(lidars[Lidar]->address, 0x01, data);
+      return data[0];
+    };
+
+/*******************************************************************************
+  async : start an acquisition
+              - with preamp enabled
+              - with DC stabilization
+
+  returns the nack error (0 if no error)
+*******************************************************************************/
+    byte async(byte Lidar = 0){
+      I2C.write(lidars[Lidar]->address, 0x00, 0x04);
+      setState(Lidar, ACQUISITION_READY);
+    };
+    
+/*******************************************************************************
+  asyncAll
+*******************************************************************************/
+    void asyncAll(){
+      for(byte i = 0; i < count; i++){
+        async(i);
+      }
+    };
+
+/*******************************************************************************
+  distance : read and
+              - with preamp enabled
+              - with DC stabilization
+
+  returns the distance in centimeter (-1 = error from I2C)
+*******************************************************************************/
+    int distance(byte Lidar, int * data){
+      byte distanceArray[2];
+      byte nackCatcher = I2C.readWord(lidars[Lidar]->address, 0x8f, distanceArray);
+      int distance = (distanceArray[0] << 8) + distanceArray[1];
+      setState(Lidar, ACQUISITION_DONE);
+      *data = distance;
+      return nackCatcher;
+    };
+
+    void setState(byte Lidar = 0, LIDAR_STATE _lidar_state = UNKNOWN){
+      lidars[Lidar]->lidar_state = _lidar_state;
+    };
+
+    LIDAR_STATE getState(byte Lidar = 0){
+      return lidars[Lidar]->lidar_state;
+    };
+
+    int distanceAndAsync(byte Lidar, int * data){
+      byte nackCatcher = distance(Lidar, data);
+      async(Lidar);
+      lidars[Lidar]->lidar_state = ACQUISITION_READY;
+      return nackCatcher;
+    };
+
+    void resetToReset(){
+      for(byte i = 0; i < MAX_LIDARS; i++){
+        toReset[i] = 0;
+      }
+    }
+
+    void addToReset(byte Lidar = 0){
+      toReset[Lidar] = 1;
+      preReset(Lidar);
+    }
+
+    void removeToReset(byte Lidar = 0){
+      toReset[Lidar] = 0;
+      postReset(Lidar);
+    }
+
+    bool checkAllToReset(){
+      for(byte i = 0; i < MAX_LIDARS; i++){
+        if(toReset[i]){
+          switch(lidars[i]->lidar_state){
+           case WAIT_AFTER_RESET : 
+              return false;
+           case NEED_RESET : 
+              return false;
+           case WAIT_AFTER_RESET_DONE : 
+              break;
+           default : 
+              //error ? 
+              break; 
+          }
+        }
+      }
+      return true;
+    }
+
+/*******************************************************************************
+  reset : Reset an I2C Lidar sending an I2C packet
+
+  Software : Write 0x00 at the 0x00 register
+  Use this when you need to reset the device (the Lidar sends only wrong data)
+*******************************************************************************/
+    void preReset(byte Lidar = 0){
+      byte _lidar = lidars[Lidar]->address;
+      I2C.write(_lidar, 0x00, 0x00);
+      if(_lidar != 0x62)
+        changeAddress(Lidar);
+      lidars[Lidar]->off();
+      delayMicroseconds(10);
+      lidars[Lidar]->on();
+      lidars[Lidar]->timer_update();
+
+      setState(Lidar, WAIT_AFTER_RESET);
+    };
+
+/*******************************************************************************
+  postReset : 
+*******************************************************************************/
+    void postReset(byte Lidar = 0){
+      byte _lidar = lidars[Lidar]->address;
+      changeAddress(Lidar);
+      configure(Lidar);
+    };
+
+    void changeAddressesToReset(){
+      if(!checkAllToReset()){
+        Serial.println("\n All lasers to reset are not ready");
+        return;
+      }
+
+      // disable all
+      for(byte i = 0; i < MAX_LIDARS; i++){
+        if(toReset[i]){
+          lidars[i]->disable();
+        }
+      }
+
+      // enable, one per one, and change address
+      for(byte i = 0; i < MAX_LIDARS; i++){
+        if(toReset[i]){
+          lidars[i]->enable();
+          delayMicroseconds(10);
+          postReset(i);
+        }
+      }
+      resetToReset();
+      return;
+    }
+    
+    // Public vars
+    // Just to make it easier
+    KFilter* filters[MAX_LIDARS];
+
+  private:
+    // 0 is not to reset, 1 is to reset
+    byte toReset[MAX_LIDARS];
+    LidarObject* lidars[MAX_LIDARS];
+    byte count = 0;
+    
+};
+
+
+#endif
